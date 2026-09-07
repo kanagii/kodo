289cf73528f9ed1bed7383da96ab36bfe6c409ef
kanagii <bluesix477@gmail.com>
2026-09-07 19:07:00 +0800

    improved design

--- config/settings.py
+++ config/settings.py
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
 LOGIN_REDIRECT_URL = "landing"
 LOGOUT_REDIRECT_URL = "landing"
 LOGIN_URL = "login"
